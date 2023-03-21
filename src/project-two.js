c78c3b3f24946c72ed5c5496c47eb1a9c649b6e4
jackvanzandt <jvv5308@psu.edu>
2023-03-21 18:53:42 -0400

    good

--- src/project-two.js
+++ src/project-two.js
@@ -1,6 +1,7 @@
 import { LitElement, html, css } from 'lit';
 
-
+import "@lrnwebcomponents/simple-icon/simple-icon.js";
+import "@lrnwebcomponents/simple-icon/lib/simple-icons.js";
 
 class ProjectTwo extends LitElement {
   static properties = {
