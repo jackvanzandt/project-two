e8787fce241af79e6da5855b6d882e01bccd9edb
jackvanzandt <jvv5308@psu.edu>
2023-03-21 18:50:22 -0400

    cmt 2

--- src/project-two.js
+++ src/project-two.js
@@ -7,21 +7,12 @@ class ProjectTwo extends LitElement {
     header: { type: String },
   }
 
-  static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--project-two-background-color);
-    }
-  `;
+  static get styles() {
+    return css`
+
+
+    `;
+  }
 
   constructor() {
     super();
@@ -29,7 +20,9 @@ class ProjectTwo extends LitElement {
 
   render() {
     return html`
-      
+
+
+
     `;
   }
 }
