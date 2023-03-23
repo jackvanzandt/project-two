fb5a24e7cfd576c7e86830dfb8286ff481314a06
jackvanzandt <jvv5308@psu.edu>
2023-03-23 13:07:08 -0400

    cmt

--- src/project-two.js
+++ src/project-two.js
@@ -3,9 +3,25 @@ import { LitElement, html, css } from 'lit';
 import "@lrnwebcomponents/simple-icon/simple-icon.js";
 import "@lrnwebcomponents/simple-icon/lib/simple-icons.js";
 
-class ProjectTwo extends LitElement {
-  static properties = {
-    header: { type: String },
+export class ProjectTwo extends LitElement {
+  static get properties() {
+    return {
+      badgeName: {
+        type: String,
+        reflect: true,
+        attribute: "badge-name",
+      },
+      badgeMajor: {
+        type: String,
+        reflect: true,
+        attribute: "badge-major",
+      },
+      badgeCreator: {
+        type: String,
+        reflect: true,
+        attribute: "badge-creator",
+      },
+    }
   }
 
   static get styles() {
@@ -17,11 +33,13 @@ class ProjectTwo extends LitElement {
 
   constructor() {
     super();
+    this.badgeCreator = "";
+    this.badgeMajor = "";
+    this.badgeName = "";
   }
 
   render() {
     return html`
-
 
 
     `;
