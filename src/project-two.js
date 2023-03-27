a984089e69036a38563f60a17e7d255f6b87b730
jackvanzandt <jvv5308@psu.edu>
2023-03-27 18:49:25 -0400

    updates

--- src/project-two.js
+++ src/project-two.js
@@ -26,7 +26,14 @@ export class ProjectTwo extends LitElement {
 
   static get styles() {
     return css`
-
+  .badge {
+    border: 1px solid black;
+			padding: 10px;
+			font-size: 20px;
+			text-align: center;
+			margin: 10px;
+			width: 200px;
+  }
 
     `;
   }
