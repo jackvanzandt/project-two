d96bdc30ece57e10edb67ce186a2866aec1854aa
jackvanzandt <jvv5308@psu.edu>
2023-04-02 20:17:04 -0400

    yee

--- api/badge-back.js
+++ api/badge-back.js
@@ -13,17 +13,9 @@ export default async function handler(request, res) {
       creator: "Donnald J. Trump",
       image: "https://badgesapp.psu.edu/uploads/badge/image/337/APA_Style.png",
       department: "Political Science"
-      name: "Badge Thing",
-      creator: "Steve",
-      image: "https://badgesapp.psu.edu/uploads/badge/image/337/APA_Style.png",
-      department: "IST"
+      
     },
-    {
-      name: "Badge stuff",
-      creator: "Donnald J. Trump",
-      image: "https://badgesapp.psu.edu/uploads/badge/image/337/APA_Style.png",
-      department: "Political Science"
-    },
+    
     {
       name: "Hungry asf",
       creator: "Caleb Pellerite",
@@ -31,21 +23,11 @@ export default async function handler(request, res) {
       department: "Food"
     },
     {
-      name: "Hungry asf",
-      creator: "Caleb Pellerite",
-      image: "https://badgesapp.psu.edu/uploads/badge/image/337/APA_Style.png",
-      department: "Food"
       name: "??????",
       creator: "someone",
       image: "https://badgesapp.psu.edu/uploads/badge/image/337/APA_Style.png",
       department: "We dont know"
     },
-    {
-    name: "??????",
-      creator: "someone",
-      image: "https://badgesapp.psu.edu/uploads/badge/image/337/APA_Style.png",
-      department: "We dont know"
-    }
     
   ];
   
